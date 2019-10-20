662d55cdca06f80ac30b86e285b384ea7cbbdf74
Jeanluc Kabulu <jeanluc.kabulu@gmail.com>
2019-10-20 18:52:36 +0200

    [Update] Adding Device orientation

---
+++ lib/app.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'home.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.amber,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyCustomForm(),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  GoogleSignInAccount _currentUser;
+  final google_blue = const Color(0xFF4285F4);
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account) {
+      setState(() {
+        _currentUser = account;
+      });
+      if (_currentUser != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      }
+    });
+    googleSignIn.signInSilently();
+  }
+
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future<FirebaseUser> _handleSignIn() async {
+    final GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final FirebaseUser user = await auth.signInWithCredential(credential);
+    setState((){
+      HomeScreen();
+    });
+    return user;
+  }
+
+  Widget signInButton(){
+   return new Container(
+      child: new RaisedButton(
+        onPressed: () => _handleSignIn().then((FirebaseUser user) => print(user)).catchError((e) => print(e)),
+        padding: EdgeInsets.only(top: 3.0,bottom: 3.0,left: 3.0),
+        color: const Color(0xFF4285F4),
+        child: new Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            new Image.asset(
+              'assets/images/google-logo.jpg',
+              height: 52.0,
+            ),
+            new Container(
+              padding: EdgeInsets.only(left: 10.0,right: 10.0),
+                child: new Text("Sign in with Google",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),)
+            ),
+          ],
+        )
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(32.0),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Lucy',
+                style: TextStyle(fontSize: 32.0, color: Colors.white, fontFamily: "Product Sans",),
+              ),
+            ),
+            Image.asset(
+              'assets/images/lucy-logo-sm.png',
+              width: 300.0,
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+            signInButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
