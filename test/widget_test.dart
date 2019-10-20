37ea07ebfb37d8fff4c0d9a8a97c3e6de6203b1b
Jeanluc Kabulu <jeanluc.kabulu@gmail.com>
2019-10-20 18:53:25 +0200

    FIXBUG : import app.dart in the  test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lucy/main.dart';
+// import 'package:lucy/main.dart';
+import 'package:lucy/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
