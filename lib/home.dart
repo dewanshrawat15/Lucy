c81b9dd0b54871c6d71573b0c2f4d43f3abee30a
Dewansh Rawat <dewanshrawat15@gmail.com>
2020-02-29 10:36:25 +0530

    Details can be fetched, inserted and deleted in sync with FireStore in realtime

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:intl/intl.dart';
+import 'package:barcode_scan/barcode_scan.dart';
 
 class HomeScreen extends StatefulWidget {
 
@@ -14,26 +15,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  Future getRecords() async{
-    List<Map> records = List();
-    Map recMap;
-    var collectionOfRecords = Firestore.instance.collection("records").where("email", isEqualTo: widget.email);
-    collectionOfRecords.snapshots().listen((data) => data.documents.forEach((doc){
-      recMap = {
-        "url": doc.data["url"],
-        "time": doc.data["time"]
-      };
-      records.add(recMap);
-    }));
-    Map result = {
-      "records": records,
-    };
-    setState(() {
-      
-    });
-    return result;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,57 +29,101 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
       ),
       body: StreamBuilder(
-        stream: getRecords().asStream(),
+        stream: Firestore.instance.collection("records").where("email", isEqualTo: widget.email).snapshots(),
         builder: (BuildContext context, AsyncSnapshot snapshot){
           if(snapshot.hasData){
-            return ListView.builder(
-              itemCount: snapshot.data["records"].length,
-              itemBuilder: (BuildContext context, int index){
-                return InkWell(
-                  onTap: () async{
-                    var tempUrl = snapshot.data["records"][index]["url"];
-                    if(await canLaunch(tempUrl)){
-                      launch(tempUrl);
-                    }
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(0),
-                    child: ListTile(
-                      title: Text(
-                        snapshot.data["records"][index]["url"],
-                        style: TextStyle(
-                          fontFamily: "Product Sans"
-                        ),
-                      ),
-                      subtitle: Text(
-                        snapshot.data["records"][index]["time"],
-                        style: TextStyle(
-                          fontFamily: "Product Sans"
+            if(snapshot.data.documents.length != 0){
+              return ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (BuildContext context, int index){
+                  return Dismissible(
+                    key: Key(snapshot.data.documents[index].documentID),
+                    onDismissed: (direction){
+                      Firestore.instance.collection("records").document(snapshot.data.documents[index].documentID).delete();
+                      Scaffold.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            "Record deleted"
+                          )
+                        )
+                      );
+                    },
+                    background: Container(color: Colors.amber),
+                    child: InkWell(
+                      onTap: () async{
+                        var tempUrl = snapshot.data.documents[index]["url"];
+                        if(await canLaunch(tempUrl)){
+                          launch(tempUrl);
+                        }
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.all(0),
+                        child: ListTile(
+                          title: Text(
+                            snapshot.data.documents[index]["url"],
+                            style: TextStyle(
+                              fontFamily: "Product Sans"
+                            ),
+                          ),
+                          subtitle: Text(
+                            snapshot.data.documents[index]["time"],
+                            style: TextStyle(
+                              fontFamily: "Product Sans"
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              }
-            );
+                  );
+                }
+              );
+            }
+            else{
+              return Align(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Center(
+                      child: Text(
+                        "No Records found",
+                        style: TextStyle(
+                          fontFamily: "Product Sans",
+                          fontSize: 24
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 3 - 42,
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset(
+                        "assets/images/panda.png",
+                        alignment: Alignment.bottomCenter,
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }
           }
-          return Center(child: CircularProgressIndicator());
+          return Center(
+            child: CircularProgressIndicator()
+          );
         }
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () async{
+          var url = await BarcodeScanner.scan();
           var collectionRef = Firestore.instance.collection("records");
           var time = DateFormat("dd-MM-yyyy hh:mm:ss").format(DateTime.now());
           Map<String, dynamic> data = {
             "name": widget.name,
             "email": widget.email,
-            "url": "instagram.com",
+            "url": url,
             "time": time
           };
           collectionRef.add(data);
-          setState(() {
-            
-          });
         },
         child: Icon(Icons.camera_alt),
       ),
